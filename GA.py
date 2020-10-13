eab811b32111596ab0e3ed33466c3e8a3d7d83be
Aditya Ramadhan <46802253+eyditye@users.noreply.github.com>
2020-10-13 19:38:40 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -15,3 +15,12 @@ def generatePopulasi(uk_pop):
     for i in range(uk_pop):
         pop.append(generateKromosom(6))
     return pop
+def encodingKromosom(krom): 
+    x1= -1+ (((2+1)/(2**-1 + 2**-2+2**-3))*((krom[0]*2**-1)+(krom[1]*2**-2)+(krom[3]*2**-3)))
+    x2 = -1 + (((1+1)/(2**-1 + 2**-2+2**-3))*((krom[4]*2**-1)+(krom[5]*2**-2)+(krom[6]*2**-3)))
+    
+    return [x1, x2]
+
+popsize = 50
+populasi = generatePopulasi(popsize)
+print(populasi)
